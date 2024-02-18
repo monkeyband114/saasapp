ff05865a10d43d50cf1a3346613cb75b7bf67229
monkeyband114 <gadocer54@gmail.com>
2024-02-18 10:31:45 +0100

    tailwind config done 	new file:   package-lock.json 	modified:   package.json 	modified:   tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -69,10 +70,11 @@ const config: Config = {
         }
       },
       animation:{
-        "accordion-up" : "accordion-up 0.2s easy-out",
+        "accordion-up" : "accordion-up 0.2s ease-out",
+        "accordion-down" : "accordion-down 0.2s ease-out",
       }
     }
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 };
 export default config;
