f515d08bbe16eb158d19b974b260146b9260418b
monkeyband114 <gadocer54@gmail.com>
2024-02-18 09:40:35 +0100

    Changes to be committed: 	deleted:    package-lock.json 	modified:   package.json 	modified:   src/app/page.tsx 	modified:   src/components/MaxWidthWrapper.tsx 	modified:   src/lib/utils.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <MaxWidthWrapper className="bmb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden">
+         
+      </div>
+    </MaxWidthWrapper>
   );
 }
+ 
