b9099a77dc9c3d43867d7bb45230d8637974ff1e
monkeyband114 <gadocer54@gmail.com>
2024-02-19 09:57:57 +0100

    homepage: 	new file:   components.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/globals.css 	modified:   src/app/page.tsx 	new file:   src/components/ui/button.tsx 	new file:   src/lib/utils.ts 	modified:   tailwind.config.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
+    <>
     <MaxWidthWrapper className="bmb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
       <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border
        border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
@@ -10,7 +15,21 @@ export default function Home() {
           Din is now Live!
          </p>
       </div>
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+            Chat with your <span className="text-blue-600">documents</span> in seconds.
+      </h1>
+      <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+        Din allows you to extrapulate insights and main points from your documents. 
+        Simply upload your file to Din to get started.
+      </p>
+      <Link className={buttonVariants({
+        size: 'lg',
+        className: 'mt-5',
+      })} href='/dashboard' target="_blank">
+        Get Started <ArrowRight className="ml-2 h-5 w-5"/>
+      </Link>
     </MaxWidthWrapper>
+    </>
   );
 }
  
